c1e737dcbb407a707772d7bcd698784079c160e4
kevindln11 <118152261+kevindln11@users.noreply.github.com>
2023-02-14 11:13:12 -0800

    up

--- kamar.php
+++ kamar.php
@@ -137,9 +137,7 @@ include "includes/koneksi.php";
                     <div class="col-xl-8 col-md-7 col-lg-9">
                         <p class="copy_right">
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>
-                                document.write(new Date().getFullYear());
-                            </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            Copyright By Rekayasa Perangkat Lunak &copy;
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </div>
 
